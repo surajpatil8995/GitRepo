381ce7ac889f8fe6e01c06e2d6bd5df1db2b0dc7
surajpatil538254 <surajpatil538254@gmail.com>
2019-12-06 13:11:25 +0000

    Code commited

--- calculator.c
+++ calculator.c
@@ -1,12 +1,12 @@
   #include <stdio.h>
     int main() {
-        char operator;
+        char op;
         double first, second;
         printf("Enter an operator (+, -, *,): ");
-        scanf("%c", &operator);
+        scanf("%c", &op);
         printf("Enter two operands: ");
         scanf("%lf %lf", &first, &second);
-        switch (operator) {
+        switch (op) {
         case '+':
             printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
             break;
