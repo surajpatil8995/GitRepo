d3c7a98dc80c2cac300df3f5b65a9de24095b13b
surajpatil538254 <surajpatil538254@gmail.com>
2019-12-06 12:29:20 +0000

    Calculator code Commitied

---
+++ calculator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int i =10;
+    int j=20;
+
+    int add=i+j;
+    float diff=i-j;
+    float prod=i*j;
+    float div=i/j;
+
+    printf("Sum = %d",add);
+    printf("Difference = %f",diff);
+    printf("Product = %f" , prod);
+    printf("Division = %f",div);
+
+    return 0;
+}
